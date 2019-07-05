91c58bc5a4a0ecbaf073927bef318f688da46d3c
zaru <sakuraba@basicinc.jp>
2019-07-05 14:20:06 +0900

    fix

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ dk run --rm -p 8080:8080 cloudrun-sample-node
 
 ```
 gcloud config set project cloudrun-sample-245604
+gcloud builds submit --config cloudbuild.yaml
 gcloud builds submit --tag gcr.io/cloudrun-sample-245604/cloudrun-sample-node
 gcloud beta run deploy --image gcr.io/cloudrun-sample-245604/cloudrun-sample-node --platform managed --memory 2Gi
 ```
--- index.js
+++ index.js
@@ -4,19 +4,8 @@
   const app = express();
   // const puppeteer = require('puppeteer');
   // const puppeteer = require('puppeteer-firefox');
-  // const browser = await puppeteer.launch({
-    // headless: true,
-    // args: [
-    //   '--no-sandbox',
-    //   '--disable-setuid-sandbox'
-    // ]
-  // });
-  // const foxr = require('foxr').default;
-  // const browser = await foxr.launch({
-  //   // executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox-bin'
-  //   executablePath: '/usr/bin/firefox'
-  // });
-  // const sleep = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout))
+  const foxr = require('foxr').default;
+  const sleep = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout))
 
   const {Storage} = require('@google-cloud/storage');
 
@@ -42,36 +31,54 @@
   });
 
   // app.get('/sc', async (req, res) => {
+  //   const browser = await puppeteer.launch({
+  //     headless: true,
+  //     args: [
+  //       '--no-sandbox',
+  //       '--disable-setuid-sandbox'
+  //     ]
+  //   });
   //   const page = await browser.newPage();
   //   await page.goto('https://example.com');
-  //   const file = await page.screenshot({encoding: 'binary'});
-  //   res.setHeader('Cache-Control', 'public, max-age=0');
-  //   res.type('png');
-  //   res.send(file);
-  //   // await page.screenshot({path: '/tmp/hoge.png'});
-  //   // res.sendFile('/tmp/hoge.png');
-  // });
-
-  // app.get('/noco', async (req, res) => {
-  //   const page = await browser.newPage();
-  //   // page.setViewport({width: 1200, height: 630, deviceScaleFactor: 1});
-  //   // await page.goto('http://localhost:9999/sample.html').catch(e => console.log(e));
-  //   await page.goto('https://noco.fun/ogp_render/b/t1Vu8I15FvKZpP6REzKj/e/ArHvhis7pI2JV3Ovv6Mt/', {
-  //     waitUntil: 'networkidle2'
-  //   }).catch(e => console.log(e));
-  //   // await page.waitFor(5000);
-  //   // await page.evaluateHandle('document.fonts.ready');
-  //   await sleep(5000);
-  //   await page.screenshot({path: '/tmp/hoge.png'});
-  //   res.sendFile('/tmp/hoge.png');
   //   // const file = await page.screenshot({encoding: 'binary'});
   //   // res.setHeader('Cache-Control', 'public, max-age=0');
   //   // res.type('png');
   //   // res.send(file);
+  //   await page.screenshot({path: '/tmp/hoge.png'});
+  //   res.sendFile('/tmp/hoge.png');
   // });
 
+  app.get('/noco', async (req, res) => {
+    console.log('noco1');
+    const execSync = require('child_process').execSync;
+    const result =  execSync('ls -la /usr/bin').toString();
+    console.log(result);
+    const browser = await foxr.launch({
+      // executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox-bin'
+      executablePath: '/usr/lib/firefox/firefox'
+    });
+    console.log('noco2');
+    const page = await browser.newPage();
+    console.log('noco3');
+    // page.setViewport({width: 1200, height: 630, deviceScaleFactor: 1});
+    // await page.goto('http://localhost:9999/sample.html').catch(e => console.log(e));
+    await page.goto('https://example.com/').catch(e => console.log(e));
+    // await page.goto('https://noco.fun/ogp_render/b/t1Vu8I15FvKZpP6REzKj/e/ArHvhis7pI2JV3Ovv6Mt/').catch(e => console.log(e));
+    // await page.waitFor(5000);
+    // await page.evaluateHandle('document.fonts.ready');
+    // await sleep(5000);
+    console.log('noco4');
+    await page.screenshot({path: '/tmp/hoge.png'});
+    res.sendFile('/tmp/hoge.png');
+    // const file = await page.screenshot({encoding: 'binary'});
+    // res.setHeader('Cache-Control', 'public, max-age=0');
+    // res.type('png');
+    // res.send(file);
+  });
+
   const port = process.env.PORT || 8080;
-  app.listen(port, () => {
+  app.listen(port, err => {
+    if (err) return console.error(err);
     console.log('Hello world listening on port', port);
   });
 })();
